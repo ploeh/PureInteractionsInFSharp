01f47734bee0ee1e8a788f48eefdb35e9773cb30
Mark Seemann <mark@ploeh.dk>
2017-06-08 21:52:17 +0200

    Implemented hello world, AST, no syntactic sugar

---
+++ ReservationClient/CommandLine.fs
@@ -0,0 +1,9 @@
+﻿namespace Ploeh.Samples
+
+type CommandLineInstruction<'a> =
+| ReadLine of (string -> 'a)
+| WriteLine of string * 'a
+
+type CommandLineProgram<'a> =
+| Free of CommandLineInstruction<CommandLineProgram<'a>>
+| Pure of 'a
--- ReservationClient/Program.fs
+++ ReservationClient/Program.fs
@@ -1,4 +1,22 @@
-﻿[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
+﻿module Ploeh.Samples.Program
+
+open System
+
+let rec interpret = function
+    | Pure x -> x
+    | Free (ReadLine  next) -> Console.ReadLine () |> next |> interpret
+    | Free (WriteLine (s, next)) ->
+        Console.WriteLine s
+        next |> interpret
+
+[<EntryPoint>]
+let main _ =
+    let program =
+        Free (WriteLine (
+                "Please enter your name.",
+                Free (ReadLine (
+                        fun s -> Free (WriteLine (
+                                        sprintf "Hello, %s!" s,
+                                        Pure ()))))))
+    interpret program
     0 // return an integer exit code
